8d97f45a2bc0341272f665bb2ee607cea77a33db
augustfr <68298953+augustfr@users.noreply.github.com>
2022-12-02 00:09:17 -0800

    add sentient claim page

---
+++ src/pages/sentient-claim.js
@@ -0,0 +1,140 @@
+import React from "react";
+import { useEffect, useState } from "react";
+import { connectWallet, getCurrentWalletConnected, claim, memberCheck} from "../utils/interact.js";
+
+const SentientClaim = () => {
+
+  let selection = document.querySelector('select');
+  if (selection) {
+    selection.addEventListener('change',() => {
+      setStatus()
+    });
+  }
+  let overallID
+
+  const [walletAddress, setWallet] = useState("")
+  const [status, setStatus] = useState("")
+  const [status2, setStatus2] = useState("")
+
+  function addWalletListener() {
+    if (window.ethereum) {
+      overallID = document.getElementById('project').value
+      window.ethereum.on("accountsChanged", (accounts) => {
+        if (accounts.length > 0) {
+          setWallet(accounts[0]);
+          setStatus("");
+        } else {
+          setWallet("");
+          setStatus("🦊 Connect to Metamask using the top right button.");
+        }
+      });
+    } else {
+      setStatus(
+        <p>
+          {" "}
+          🦊{" "}
+          <a target="_blank" href={`https://metamask.io/download.html`}>
+            You must install Metamask, a virtual Ethereum wallet, in your
+            browser.
+          </a>
+        </p>
+      );
+    }
+  }
+  
+  useEffect(async () => {
+    const {address, status} = await getCurrentWalletConnected();
+    setWallet(address)
+    setStatus(status);
+
+    addWalletListener(); 
+}, [])
+
+  const connectWalletPressed = async () => {
+    const walletResponse = await connectWallet();
+    setStatus(walletResponse.status);
+    setWallet(walletResponse.address);
+  }
+
+  const onCheckPressed = async () => {
+    let membershipID = document.getElementById('membershipCheck').value
+    if (membershipID > 49 || membershipID == '') {
+      setStatus2('Please enter a valid Sentient membershipID (0-49)')
+      return
+    } else {
+      setStatus2('')
+    }
+    await memberCheck(membershipID)
+  }
+
+  const onClaimPressed = async () => {
+    overallID = document.getElementById('project').value
+    if (overallID == 0) {
+      setStatus("Select an artist from the dropdown menu above")
+      return
+    }
+    let numToMint = document.getElementById('quantity').value
+    let membershipID = document.getElementById('membership').value
+    let projectID = document.getElementById('project').value
+    if (membershipID > 49) {
+      setStatus('Please enter a valid Sentient membershipID (0-49)')
+      return
+    }
+    if (numToMint == '') {
+      numToMint = 1
+    }
+    const { status } = await claim(projectID, membershipID, numToMint)
+    setStatus(status)
+  }
+
+
+  return (
+    <div className="Minter">
+      <button id="walletButton" onClick={connectWalletPressed}>
+        {walletAddress.length > 0 ? (
+          "Connected: " +
+          String(walletAddress).substring(0, 6) +
+          "..." +
+          String(walletAddress).substring(38)
+        ) : (
+          <span>Connect Wallet</span>
+        )}
+      </button><br></br><br></br><br></br><br></br>
+          <select name="project" id="project">
+            <option value="0">Select Project</option>
+            <option value="1">SOMNAI (Otherwhere)</option>
+            <option value="2">Roope Rainisto (ANIMA)</option>
+            <option value="3">Austiin (Remnants)</option>
+            <option value="4">Revrart (Voyage)</option>
+            <option value="5">Saucebook (Embracing Chaos)</option>
+            <option value="6">Rikkar (Yūgen)</option>
+            <option value="7">Huemin (Seek)</option>
+            <option value="8">Inner_Sanctum & Pancakes (MOODs)</option>
+            <option value="9">H01 & DeltaSauce (Nexus)</option>
+            <option value="10">Claire Silver (Page)</option>
+            <option value="11">MrHabMo (Esquisse)</option>
+            <option value="12">KaptN (Noah's Ark)</option>
+            <option value="13">Ren AI (Memory)</option>
+            <option value="1000">Dreamers</option>
+          </select><br></br>
+          <br></br><br></br><br></br>
+          <input id='membership' type='text' placeholder='Membership ID (0-49)' style={{width: '175px'}}/> &nbsp;&nbsp;&nbsp;
+          <input id='quantity' type='text' placeholder='1' style={{width: '25px'}}/>&nbsp;&nbsp;&nbsp;
+          <button id = "claim" onClick={onClaimPressed} className = 'btn walletButton'>Claim</button><br></br><br></br>
+          <p id="status">
+            {status}
+          </p>
+          <br></br>
+          <br></br>
+          <input id='membershipCheck' type='text' placeholder='Membership ID (0-49)' style={{width: '175px'}}/>&nbsp;&nbsp;&nbsp;
+          <button id = "check" onClick={onCheckPressed} className = 'btn walletButton'>Check</button><br></br>
+      <p id="status2">
+        {status2}
+      </p>
+      <p id="list"></p>
+      
+    </div>
+  );
+};
+
+export default SentientClaim;
--- src/utils/interact.js
+++ src/utils/interact.js
@@ -3,8 +3,10 @@ const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = createAlchemyWeb3(alchemyKey);
 
-const curatedContractABI = require('../curated-abi.json')
-const curatedContractAddress = "0xb7ec7bbd2d2193b47027247fc666fb342d23c4b5";
+export const curatedContractABI = require('../curated-abi.json')
+export const curatedContractAddress = "0xb7ec7bbd2d2193b47027247fc666fb342d23c4b5";
+export const dreamersABI = require('../dreamers-abi.json')
+export const dreamersContract = "0xfd29cdc7868cacece537571bd0c879df55f2bc51";
 
 export async function getTokenInfo(projectID) {
     window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress);
@@ -13,6 +15,33 @@ export async function getTokenInfo(projectID) {
     data: window.contract.methods.projectTokenInfo(projectID).encodeABI()
   }).then(data => tokenInfo = data);
   return web3.eth.abi.decodeParameters(curatedContractABI[36].outputs, await tokenInfo)
+}
+
+export async function getProjectDetails(projectID) {
+  window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress);
+  let projectDetails = web3.eth.call({
+  to: curatedContractAddress,
+  data: window.contract.methods.projectDetails(projectID).encodeABI()
+}).then(data => projectDetails = data);
+return web3.eth.abi.decodeParameters(curatedContractABI[37].outputs, await projectDetails)
+}
+
+export async function showProjectTokens(projectID) {
+  window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress);
+  let projectTokens = web3.eth.call({
+  to: curatedContractAddress,
+  data: window.contract.methods.projectShowAllTokens(projectID).encodeABI()
+}).then(data => projectTokens = data);
+return web3.eth.abi.decodeParameters(curatedContractABI[51].outputs, await projectTokens)
+}
+
+export async function getNextProjectID() {
+  window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress);
+  let nextProjectId = web3.eth.call({
+  to: curatedContractAddress,
+  data: window.contract.methods.nextProjectId().encodeABI()
+}).then(data => nextProjectId = data);
+return web3.eth.abi.decodeParameters(curatedContractABI[60].outputs, await nextProjectId)[0]
 }
 
 export const publicMint = async(minterContract, minterABI, projectID, numToMint) => {
@@ -40,7 +69,7 @@ export const publicMint = async(minterContract, minterABI, projectID, numToMint)
                 });
             return {
                 success: true,
-                status: "✅ Check out your transaction on Etherscan: https://goerli.etherscan.io/tx/" + txHash
+                status: "✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" + txHash
             }
         } catch (error) {
             return {
@@ -58,7 +87,7 @@ export const earlyMint = async(minterContract, minterABI, projectID, membershipI
     if (!projTokenInfo[8]) {
       return { status: "😥 Not in presale phase. Use public minting option instead!" }
     }
-    if (membershipID > 50 || membershipID == '') {
+    if (membershipID > 50 || membershipID === '') {
         return { status: "😥 Enter a valid membership ID (0-50)" }
     }
     if (numToMint > 3) {
@@ -79,7 +108,7 @@ export const earlyMint = async(minterContract, minterABI, projectID, membershipI
                 });
             return {
                 success: true,
-                status: "✅ Check out your transaction on Etherscan: https://goerli.etherscan.io/tx/" + txHash
+                status: "✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" + txHash
             }
         } catch (error) {
             return {
@@ -88,6 +117,123 @@ export const earlyMint = async(minterContract, minterABI, projectID, membershipI
             }
         }
    }
+}
+const checkDreamersClaimed = async(membershipID) => {
+  window.contract = new web3.eth.Contract(dreamersABI, dreamersContract)
+  let dreamersClaimed = web3.eth.call({
+  to: dreamersContract,
+  data: window.contract.methods.sentientClaimed(membershipID).encodeABI()
+  }).then(data => dreamersClaimed = data).catch((err) => {})
+  if (typeof(await dreamersClaimed) == 'undefined') {
+    return 0
+  } else {
+    return web3.eth.abi.decodeParameters(dreamersABI[47].outputs, await dreamersClaimed)[0]
+    }
+  }
+
+const checkIfClaimed = async(projectID, membershipID) => {
+  if (projectID == 1000) {
+    let numClaimed = await checkDreamersClaimed(membershipID)
+    if (numClaimed == 3) {
+      return { success: true }
+    } else {
+      return { success: false }
+    }
+  } else {
+    const _id = parseInt(projectID * 10000) + parseInt(membershipID)
+      window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress)
+      let ownerOf = web3.eth.call({
+        to: curatedContractAddress,
+        data: window.contract.methods.ownerOf(_id).encodeABI()})
+        .then(data => ownerOf = data).catch((err) => {})
+      if (typeof(await ownerOf) == 'undefined') {
+        return { success: false }
+      } else {
+        return { success: true }
+      }
+  }
+}
+
+export const claim = async(projectID, membershipID, numToMint) => {
+  if ((await checkIfClaimed(projectID, membershipID)).success) {
+    return { status: "😥 Already claimed" }
+  }
+  if (projectID == 1000) {
+    window.contract = new web3.eth.Contract(dreamersABI, dreamersContract)
+    const transactionParameters = {
+      to: dreamersContract,
+      from: window.ethereum.selectedAddress,
+      data: window.contract.methods.claimSentient(membershipID, numToMint).encodeABI()
+    };
+    try {
+        const txHash = await window.ethereum
+            .request({
+                method: 'eth_sendTransaction',
+                params: [transactionParameters],
+            });
+        return {
+            success: true,
+            status: "✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" + txHash
+        }
+    } catch (error) {
+        return {
+            success: false,
+            status: "😥 Something went wrong: " + error.message
+        }
+    } 
+  } else {
+      if (numToMint > 1) {
+        return {
+          success: false,
+          status: "Can only claim 1 curated artwork per project, per Sentient membership"
+        }
+      } else {
+        window.contract = new web3.eth.Contract(curatedContractABI, curatedContractAddress)
+        const transactionParameters = {
+          to: curatedContractAddress,
+          from: window.ethereum.selectedAddress,
+          data: window.contract.methods.claimSentient(projectID, membershipID).encodeABI()
+        };
+        try {
+            const txHash = await window.ethereum
+                .request({
+                    method: 'eth_sendTransaction',
+                    params: [transactionParameters],
+                });
+            return {
+                success: true,
+                status: "✅ Check out your transaction on Etherscan: https://etherscan.io/tx/" + txHash
+            }
+        } catch (error) {
+            return {
+                success: false,
+                status: "😥 Something went wrong: " + error.message
+            }
+        } 
+      }
+  }
+}
+
+export const memberCheck = async(membershipID) => {
+  document.getElementById('list').innerHTML = ''
+  let projectID = parseInt(await getNextProjectID())
+  for (let x = 1; x < projectID; x++) {
+    let count = 0;
+    let projectName = (await getProjectDetails(x))[0]
+    let myval = await showProjectTokens(x)
+    let tokenID_check = parseInt(membershipID) + (x * 10000)
+  for (let i = 0; i < myval[0].length; i++) {
+      if ((tokenID_check) == myval[0][i]) {
+          document.getElementById('list').innerHTML += projectName + ' ' + membershipID + ' has already been claimed <br>';
+          count = 1;
+      }
+    }
+    if (count == 0) {
+        document.getElementById('list').innerHTML += projectName + ' ' + membershipID + ' has NOT been claimed <br>';
+    }
+  }
+  let dreamersClaimed = await checkDreamersClaimed(membershipID);
+  document.getElementById('list').innerHTML += dreamersClaimed + '/3 Dreamers have been claimed <br>';
 }
 
 export const connectWallet = async () => {
@@ -115,7 +261,7 @@ export const connectWallet = async () => {
             <p>
               {" "}
               🦊{" "}
-              <a target="_blank" href={`https://metamask.io/download.html`}>
+              <a target="_blank" rel="noreferrer" href={`https://metamask.io/download.html`}>
                 You must install Metamask, a virtual Ethereum wallet, in your
                 browser.
               </a>
@@ -157,7 +303,7 @@ export const connectWallet = async () => {
             <p>
               {" "}
               🦊{" "}
-              <a target="_blank" href={`https://metamask.io/download.html`}>
+              <a target="_blank" rel="noreferrer" href={`https://metamask.io/download.html`}>
                 You must install Metamask, a virtual Ethereum wallet, in your
                 browser.
               </a>
