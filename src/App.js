e8a069ed5390df2f5731d9c6a37df0051d57bc8c
augustfr <68298953+augustfr@users.noreply.github.com>
2023-02-19 19:09:21 -0800

    add curated details json

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Reinventions from './pages/reinventions';
 import EbbsandFlows from './pages/ebbsandflows';
 import CuratedMinting from './pages/curated-minting';
 import SentientClaim from './pages/sentient-claim';
+import CuratedDetails from './pages/curated-details';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
     	<Route path='/ebbs-and-flows' element={<EbbsandFlows/>} />
 		<Route path='/curated-minting' element={<CuratedMinting/>} />
 		<Route path='/sentient-claim' element={<SentientClaim/>} />
+		<Route path='/curated-details' element={<CuratedDetails/>} />
 	</Routes>
 	</Router>
   );
